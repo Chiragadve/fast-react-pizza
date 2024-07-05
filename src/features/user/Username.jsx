eeeb3a467bb270bdb6a3ba233d3afe4232ed889e
Chirag <chiragadve4@gmail.com>
2024-07-05 19:32:11 +0530

    Styling With Tailwind

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Chirag</div>;
+  return <div className="hidden text-sm font-semibold md:block">Chirag</div>;
 }
 
 export default Username;
